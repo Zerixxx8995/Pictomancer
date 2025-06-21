f6c145960d3b7a69988e27df770986b0ac4d9f62
Zerixxx8995 <manavamin99zx@gmail.com>
2025-06-21 17:11:21 +0530

    creating all components pages and routing

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container=styled.div`
+height: 100vh;
+overflow-y: scroll;
+`;
+const Home = () => {
+  return (
+    <div>
+      home
+    </div>
+  )
+}
+
+export default Home
